d3080bc3ddc33a26d8d13fd36969357029bd8c29
Shu <hongshufang@hotmail.com>
2019-07-20 20:12:44 +0800

    add some solutions

--- 31. 下一个排列/31. 下一个排列.go
+++ 31. 下一个排列/31. 下一个排列.go
@@ -1,29 +1,39 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	a := []int{1, 1, 1, 0}
-	b := isOneBitCharacterTX(a)
-	fmt.Println(b)
+	nextPermutation(a)
+	//fmt.Println(b)
 	//c := maximalRectangle(a)
 	//fmt.Println(c)
 }
-func isOneBitCharacter(bits []int) bool {
-	blen := len(bits)
-	i := 0
-	for i < blen {
-		if i == blen-1 {
-			return true
-		}
-		if bits[i] == 1 {
-			i += 2
-		} else {
-			i++
-		}
-
+func nextPermutation(nums []int) {
+	i := len(nums) - 2
+	j := i + 1
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
 	}
-	return false
+	if i >= 0 {
+		for j >= 0 && nums[j] <= nums[i] {
+			j--
+		}
+		swap(nums, i, j)
+	}
+	reverse(nums, i+1)
+}
+func swap(nums []int, i int, j int) {
+	var temp int
+	temp = nums[j]
+	nums[j] = nums[i]
+	nums[i] = temp
+}
+func reverse(nums []int, begin int) {
+	var j int
+	i := begin
+	j = len(nums) - 1
+	for i < j {
+		swap(nums, i, j)
+		i++
+		j--
+	}
 }
