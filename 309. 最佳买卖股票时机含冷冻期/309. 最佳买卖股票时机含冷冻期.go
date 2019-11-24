c17774acf0d3498ebc7e75a707e02735f0f17bd5
FangHongshu <hongshufang@hotmail.com>
2019-11-24 19:42:34 +0800

    add 152

--- 309. 最佳买卖股票时机含冷冻期/309. 最佳买卖股票时机含冷冻期.go
+++ 309. 最佳买卖股票时机含冷冻期/309. 最佳买卖股票时机含冷冻期.go
@@ -3,11 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{2, 1, 1, 2}
+	a := []int{1, 2, 3, 0, 2}
 	b := maxProfit(a)
 	//c := maximalRectangle(a)
 	fmt.Println(b)
 }
 func maxProfit(prices []int) int {
-
+	sold, hold, rest := 0, 0, 0
+	for i := range prices {
+		preSold := sold
+		sold = hold + prices[i]
+		hold = max(hold, rest-prices[i])
+		rest = max(rest, preSold)
+	}
+	return max(sold, rest)
+}
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
